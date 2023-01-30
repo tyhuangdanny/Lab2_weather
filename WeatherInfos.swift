08f1d3cc90253d0ac79766d1f7e6506efbd195ae
tyhuangdanny <114411435+tyhuangdanny@users.noreply.github.com>
2023-01-30 14:04:45 +0100

    Add files via upload

---
+++ WeatherInfos.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherInfos.swift
+//  Weather
+//
+//  Created by Elsa Netz on 2022-11-24.
+//
+
+import Foundation
+
+struct WeatherInfos: Codable {
+    var timeSeries: [WeatherInfo]
+}
